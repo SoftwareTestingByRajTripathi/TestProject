eb02032229b0917a751316c46115ca4c7d755cad
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2020-10-05 13:17:42 +0530

    raj

--- src/test/java/com/testpage/WebDashBoardPage.java
+++ src/test/java/com/testpage/WebDashBoardPage.java
@@ -1,9 +1,12 @@
 package com.testpage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class WebDashBoardPage {
 	WebDriver driver;
+	@FindBy (xpath = "//a[@class='name']")WebElement dashboard;
 	
 
 }
